b2df5affb55a23851196349dc4ec4c76fc37ea47
Vincas Stonys <vincaslt@gmail.com>
2015-08-06 16:51:52 +0300

    US-4: Added REST for users

---
+++ src/main/java/lt/msi2015/user/UserDto.java
@@ -0,0 +1,26 @@
+package lt.msi2015.user;
+
+import lt.msi2015.util.Dto;
+
+public class UserDto extends Dto {
+	
+	public String email;
+	public String firstName;
+	public String lastName;
+	public String password;
+	public String passwordConfirm;
+	
+	public UserDto() {
+		super();
+	}
+
+
+	public UserDto(String email, String firstName, String lastName, String password, String passwordConfirm) {
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		this.passwordConfirm = passwordConfirm;
+	}
+
+}
