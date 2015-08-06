25b88134cb435f50177403db4a192004eade58a0
Vincas Stonys <vincaslt@gmail.com>
2015-08-06 11:36:36 +0300

    Added description field to transfer form entity

--- src/main/java/lt/msi2015/points_transfer_info/PointsTransferInfoDto.java
+++ src/main/java/lt/msi2015/points_transfer_info/PointsTransferInfoDto.java
@@ -1,15 +1,5 @@
 package lt.msi2015.points_transfer_info;
 
-import java.util.Date;
-
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.Size;
-
 import lt.msi2015.util.Dto;
 
 public class PointsTransferInfoDto extends Dto {
