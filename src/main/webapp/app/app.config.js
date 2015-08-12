6fb16f88311590597bd7b9a626b378de8629b89c
Mykolas Grinevicius <mykolas@grinevicius.lt>
2015-08-12 14:41:36 +0300

    added routes

--- src/main/webapp/app/app.config.js
+++ src/main/webapp/app/app.config.js
@@ -7,6 +7,23 @@
 	  
 	  $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
 
-  	});
+  	})
+  	.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/login");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('login', {
+    	url: "/login",
+        templateUrl: "loginPage.tmpl.html"
+      
+    })
+    .state('state2', {
+      url: "/app/profile_header",
+      templateUrl: "app/profile_header/profile_header.tmpl.html"
+    });
+});
 
 })(window.angular);
