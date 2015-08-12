90fdaf70f5a55a013ceb376463daa79d29cf1a8c
Marius Alchimavicius <alch.marius@gmail.com>
2015-08-12 18:04:26 +0300

    commented unneeded from field, minor fixes in User class

--- src/main/java/lt/msi2015/applicationSettings/ApplicationSettingsRepository.java
+++ src/main/java/lt/msi2015/applicationSettings/ApplicationSettingsRepository.java
@@ -3,8 +3,11 @@ package lt.msi2015.applicationSettings;
 import java.util.List;
 
 import org.springframework.data.repository.Repository;
+import java.lang.String;
+import lt.msi2015.applicationSettings.ApplicationSetting;
 
 public interface ApplicationSettingsRepository extends Repository<ApplicationSetting, String> {
 	ApplicationSetting save(ApplicationSetting setting);
 	List<ApplicationSetting> findAll();
+	ApplicationSetting findByProperty(String property);
 }
