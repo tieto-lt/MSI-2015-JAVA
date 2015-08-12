74beca36424a3b472ece28086f3dc4c7f615f0dc
Augustas Skaburskas <augustas.skaburskas@gmail.com>
2015-08-12 17:37:41 +0300

    edited logout button

--- src/main/webapp/app/app.config.js
+++ src/main/webapp/app/app.config.js
@@ -33,6 +33,10 @@
 		    .state('userPage', {
 		    	url: "/user",
 		    	templateUrl: "userPage.tmpl.html"
+		    })
+			.state('logout', {
+		    	url: "/logout",
+		    	templateUrl: "loginPage.tmpl.html"
 		    });
 		});
 
