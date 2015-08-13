21e59fb6739d7255068fca77190ad8ee009f9dbe
Augustas Skaburskas <augustas.skaburskas@gmail.com>
2015-08-13 18:39:46 +0300

    added pasword hashing

--- src/main/java/lt/msi2015/security/SecurityConfiguration.java
+++ src/main/java/lt/msi2015/security/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
@@ -23,7 +24,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-        	.userDetailsService(userDetailsService);
+        	.userDetailsService(userDetailsService)
+        	.passwordEncoder(new BCryptPasswordEncoder());
     }
 	
   @Override
--- src/main/java/lt/msi2015/user/UserRest.java
+++ src/main/java/lt/msi2015/user/UserRest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -42,7 +43,7 @@ public class UserRest {
 			info.email,
 			info.firstName,
 			info.lastName,
-			info.password,
+			new BCryptPasswordEncoder().encode(info.password),
 			monthlyLimit
 			)
 		);
