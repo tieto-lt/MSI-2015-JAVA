120955d7e92a69814cf15b646df60921ced181b5
Augustas Skaburskas <augustas.skaburskas@gmail.com>
2015-08-13 10:43:56 +0300

    edited points form validation

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into user(email, first_name, last_name, password, date_created, role) values ('user1@oho.lt', 'Agnė', 'Jegova', 'password1', '2015.05.05', '1');	
 insert into user(email, first_name, last_name, password, date_created, role) values ('user2@oho.lt', 'Marius', 'Alchimavicius', 'password2', '2015.02.06', '0');	
 insert into user(email, first_name, last_name, password, date_created, role) values ('user3@oho.lt', 'Var', 'Gar', 'password3', '2013.06.13', '0');	
-insert into application_setting(property, value) values ('one_time_limit', 100);
-insert into application_setting(property, value) values ('monthly_limit', 1000);
+insert into application_setting(property, value) values ('one_time_limit', 50);
+insert into application_setting(property, value) values ('monthly_limit', 200);
