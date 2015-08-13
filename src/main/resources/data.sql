8d6ebbee70657a4e0b663e014681adee0cc00824
Mykolas Grinevicius <mykolas@grinevicius.lt>
2015-08-13 16:21:11 +0300

    Made leaderboard to select leaders from 1st day of the month to last (30, 31 ...)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,13 @@
-insert into user(email, first_name, last_name, password, date_created, role) values ('user1@oho.lt', 'Agnė', 'Jegova', 'password1', '2015.05.05', '1');	
-insert into user(email, first_name, last_name, password, date_created, role) values ('user2@oho.lt', 'Marius', 'Alchimavicius', 'password2', '2015.02.06', '0');	
-insert into user(email, first_name, last_name, password, date_created, role) values ('user3@oho.lt', 'Var', 'Gar', 'password3', '2013.06.13', '0');	
+insert into user(email, first_name, last_name, password, date_created, role, points_to_give) values ('user1@oho.lt', 'Agnė', 'Jegova', 'password1', '2015.05.05', '1', 1000);	
+insert into user(email, first_name, last_name, password, date_created, role, points_to_give) values ('user2@oho.lt', 'Marius', 'Alchimavicius', 'password2', '2015.02.06', '0', 1000);	
+insert into user(email, first_name, last_name, password, date_created, role, points_to_give) values ('user3@oho.lt', 'Var', 'Gar', 'password3', '2013.06.13', '0', 1000);	
 insert into application_setting(property, value) values ('one_time_limit', 100);
 insert into application_setting(property, value) values ('monthly_limit', 1000);
+
+insert into points_transfer_info(comment, date_created, from_userid, points, to_userid) values ("aaaa", '2015.05.12', 1, 100, 1);
+insert into points_transfer_info(comment, date_created, from_userid, points, to_userid) values ("aaaa", '2015.05.12', 2, 100, 1);
+insert into points_transfer_info(comment, date_created, from_userid, points, to_userid) values ("aaaa", '2015.07.02', 2, 100, 1);
+insert into points_transfer_info(comment, date_created, from_userid, points, to_userid) values ("aaaa", '2015.07.02', 3, 100, 1);
+insert into points_transfer_info(comment, date_created, from_userid, points, to_userid) values ("aaaa", '2015.08.02', 3, 100, 1);
+insert into points_transfer_info(comment, date_created, from_userid, points, to_userid) values ("aaaa", '2015.05.02', 3, 100, 1);
+insert into points_transfer_info(comment, date_created, from_userid, points, to_userid) values ("aaaa", '2015.08.30', 3, 100, 2);
