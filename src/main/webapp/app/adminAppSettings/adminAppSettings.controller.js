3f53eb85f13d17b1b61974f16ff385be5d8087de
Mykolas Grinevicius <mykolas@grinevicius.lt>
2015-08-17 11:36:45 +0300

    edited shop item entity, changed to private fields

--- src/main/webapp/app/adminAppSettings/adminAppSettings.controller.js
+++ src/main/webapp/app/adminAppSettings/adminAppSettings.controller.js
@@ -1,44 +1,55 @@
 (function(angular) {
 	'use strict';
 
-	angular
-		.module('app.adminAppSettings')
-		.controller('AdminAppSettingsController', AdminAppSettingsController);
+	angular.module('app.adminAppSettings').controller(
+			'AdminAppSettingsController', AdminAppSettingsController);
 
 	AdminAppSettingsController.$inject = [ 'AdminAppSettingsFactory' ];
-	
 
 	function AdminAppSettingsController(AdminAppSettingsFactory) {
 		var vm = this;
-		
+
 		vm.transferInfo = {
-			      monthlyLimit: '',
-			      oneTimePointsLimit: ''
-			    };
-		
+			monthlyLimit : '',
+			oneTimePointsLimit : ''
+		};
+
+		getKarmaLimits();
+
+		function getKarmaLimits() {
+			AdminAppSettingsFactory.getOneTimeLimit().then(function(response) {
+				vm.transferInfo.oneTimePointsLimit = response;
+			});
+
+			AdminAppSettingsFactory.getMonthlyLimit().then(function(response) {
+				vm.transferInfo.monthlyLimit = response;
+			});
+		}
+
 		vm.save = save;
-		
+
 		/*
 		 * Send new monthly, one time limits to server
 		 */
 		function save() {
 			vm.successMessage = '';
-	    	vm.errorMessage = '';
-	    	
-	    	AdminAppSettingsFactory
-	    		.saveSettings(vm.transferInfo)
-	    		.then(function() {
-	    			vm.successMessage = 'Transfer was successful';	    			
-	    			vm.transferInfo.monthlyLimit = '';
-	    			vm.transferInfo.oneTimePointsLimit = '';
-	    			vm.settingsForm.$setPristine();
-	    			vm.settingsForm.$setUntouched();
-	    		}, function() {
-	    			vm.errorMessage = 'Transfer failed';
-	    			vm.transferInfo.monthlyLimit = '';
-	    			vm.transferInfo.oneTimePointsLimit = '';
-	    			vm.settingsForm.$setPristine();
-	    		})
+			vm.errorMessage = '';
+
+			AdminAppSettingsFactory.saveSettings(vm.transferInfo).then(
+					function() {
+						vm.successMessage = 'Transfer was successful';
+						vm.transferInfo.monthlyLimit = '';
+						vm.transferInfo.oneTimePointsLimit = '';
+						vm.settingsForm.$setPristine();
+						vm.settingsForm.$setUntouched();
+						getKarmaLimits();
+					}, function() {
+						vm.errorMessage = 'Transfer failed';
+						vm.transferInfo.monthlyLimit = '';
+						vm.transferInfo.oneTimePointsLimit = '';
+						vm.settingsForm.$setPristine();
+						getKarmaLimits();
+					})
 		}
 	}
 })(window.angular);
