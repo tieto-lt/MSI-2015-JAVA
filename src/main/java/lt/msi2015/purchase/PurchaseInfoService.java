5c36bf78072968917fec82953fc45c2d01167943
Augustas Skaburskas <augustas.skaburskas@gmail.com>
2015-08-19 12:04:35 +0300

    US-10.7 added buy action (server-side)

--- src/main/java/lt/msi2015/purchase/PurchaseInfoService.java
+++ src/main/java/lt/msi2015/purchase/PurchaseInfoService.java
@@ -3,19 +3,37 @@ package lt.msi2015.purchase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lt.msi2015.shop.ShopItem;
+import lt.msi2015.shop.ShopRepository;
+
 @Service
 public class PurchaseInfoService {
 	
 	@Autowired
 	PurchaseInfoRepository purchaseRepo;
 	
+	@Autowired
+	ShopRepository shopRepo;
+	
 	public boolean buy(PurchaseInfoDto info) {
-
-		return purchaseRepo.save(
+		
+		if(purchaseRepo.save(
 				new PurchaseInfo(
 					info.getId(),
 					info.getUserId(),
 					info.getShopItemId())
-				) != null;
+				) != null) {
+			
+			return subShopItemAmount(info.getShopItemId());
+		} else {
+			return false;
+		}
+	}
+	
+	private boolean subShopItemAmount(Long shopItemId) {
+		ShopItem item = shopRepo.findById(shopItemId);
+		Integer itemQuantity = item.getQuantity();
+		item.setQuantity(itemQuantity - 1);
+		return shopRepo.save(item) != null;
 	}
 }
