2d60a07b8d8450e5cd812ee00f5383d0797ebdd7
Vincas Stonys <vincaslt@gmail.com>
2015-08-19 09:59:34 +0300

    Added image retrieval for items

--- src/main/webapp/app/shopItem/shopItem.controller.js
+++ src/main/webapp/app/shopItem/shopItem.controller.js
@@ -7,14 +7,20 @@
 
   ShopItemController.$inject = ['ShopItemFactory'];
 
-  function ShopItemController(ShopItemFactory ) {
+  function ShopItemController(ShopItemFactory) {
     var vm = this;
+    
+    vm.decodeImage = decodeImage;
 
 //    vm.shopItem = ShopItemFactory.getShopItem($routeParams.id);
     vm.shopItems = [];
     ShopItemFactory.getShopItems().then(function (response) {
     	vm.shopItems = response.data;
 	}) ;
+    
+    function decodeImage(image) {
+    	return atob(image);
+    }    
   }
   
 
