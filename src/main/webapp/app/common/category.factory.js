2373c85e48bf9ede730110aeb6135e5396007a35
Marius Alchimavicius <alch.marius@gmail.com>
2015-08-24 17:25:31 +0300

    added category settings in admin homepage

--- src/main/webapp/app/common/category.factory.js
+++ src/main/webapp/app/common/category.factory.js
@@ -14,24 +14,17 @@
 		
 		function loadCategories() {
 			return $http.get('api/categories').then(function(response) {
-				console.log(response.data);
 				angular.extend(categories, response.data);
 			});
 		}
 		
-		function getEnabledCategories() {
-			var enabledCategories = [];
-			categories.forEach(function(category) {
-				if (category.enabled) {
-					enabledCategories.push(category);
-				}
-			})
+		function getCategories() {
 			
-			return enabledCategories;
+			return categories;
 		}
 		
 		return {
-			getEnabledCategories: getEnabledCategories,
+			getCategories: getCategories,
 			loadCategories: loadCategories
 		};
 	}
