11628ce1c5faf7023f87400fb77d8f9cbe2eeaa2
Mykolas Grinevicius <mykolas@grinevicius.lt>
2015-08-25 16:23:51 +0300

    Fixed purchase history order

--- src/main/java/lt/msi2015/purchase/PurchaseInfoRepository.java
+++ src/main/java/lt/msi2015/purchase/PurchaseInfoRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.repository.Repository;
 public interface PurchaseInfoRepository extends Repository<PurchaseInfo, Long> {
 	PurchaseInfo save(PurchaseInfo info);
 	List<PurchaseInfo> findAll();
+	List<PurchaseInfo> findAllByUserId(Long userId);
 }
